10c54e91bf48d73e1364e5e0711daf9cb51bc810
Vegard Haneberg <vegard.haneberg@live.no>
2022-02-18 22:20:04 +0100

    added fontawesome

--- WineRegApp/AssemblyInfo.cs
+++ WineRegApp/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("FontAwesome6Regular.otf", Alias = "FontAwesome6Regular")]
+[assembly: ExportFont("FontAwesome6Brands.otf", Alias = "FontAwesome6Brands")]
+[assembly: ExportFont("FontAwesome6Solid.otf", Alias = "FontAwesome6Solid")]
